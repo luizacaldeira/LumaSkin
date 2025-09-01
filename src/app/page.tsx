4726ee9fd851bd9929cc23923130a867c08887db
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2025-08-31 23:18:35 -0300

    feat(hero-section): add title and subtitle props to HeroSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import HeroSection from '../components/layout/HeroSection';
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <HeroSection />
+      <HeroSection title="your skin, your light, everyday." subtitle="Discover the perfect blend of nature and science with our skincare products. At Luma Skin, we combine advanced dermatological research with the purest botanical ingredients to deliver visible results and long-lasting skin health." />
     </div>
   );
 }
