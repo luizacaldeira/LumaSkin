d46dbd628fbda89ecb57244fe7f7d6202d0c31b6
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2025-09-30 23:42:57 -0300

    fix(route): set dynamic export for improved rendering behavior

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { prisma, defaultSelectFields } from '../../../../prisma/database/prismaClient';
 import { getProductByName } from '@/lib/products';
 
+export const dynamic = 'force-dynamic';
 
 export async function GET(request: NextRequest) {
     try {
